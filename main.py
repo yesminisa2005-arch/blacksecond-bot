f60b19d5b92b4ebfa83a996e94f8e31189fab766
yesminisa2005-arch <yesminisa2005@gmail.com>
2026-06-18 08:13:52 +0100

    type fix webhook for render

--- main.py
+++ main.py
@@ -11,4 +11,13 @@ app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
 if __name__ == "__main__":
-    app.run_polling()
+    import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run_webhook(
+        listen="0.0.0.0",
+        port=port,
+        url_path="webhook",
+        webhook_url=os.environ['WEBHOOK_URL'] + "/webhook"
+    
