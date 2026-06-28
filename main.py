f3781aa7254ffdc04b234d39cf52ea339db1ef64
yesminisa2005-arch <yesminisa2005@gmail.com>
2026-06-28 06:54:29 +0100

    Delete main.py

--- main.py
+++
@@ -1,4 +0,0 @@
-
-# WhatsApp bot - Now using Node.js + Baileys
-# main.py deleted. See index.js and package.json instead
-OWNER_NUMBER = "2348137587041"  # Replace with your WhatsApp number + country code
